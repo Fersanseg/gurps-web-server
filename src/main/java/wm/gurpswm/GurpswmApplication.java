35f77bbfa144b18127e5a9b7ad75c77d919f14f4
Fersanseg <fer_ssa94@hotmail.com>
2024-02-27 20:42:29 +0100

    Trying to get Render to work

--- src/main/java/wm/gurpswm/GurpswmApplication.java
+++ src/main/java/wm/gurpswm/GurpswmApplication.java
@@ -18,7 +18,7 @@ public class GurpswmApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("https://localhost:4200");
+				registry.addMapping("/**").allowedOrigins("*");
 			}
 		};
 	}
