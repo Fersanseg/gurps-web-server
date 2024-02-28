bfb3e309c0cf063a26e6d3d75159a715e4bf0282
Fersanseg <fer_ssa94@hotmail.com>
2024-02-28 20:33:43 +0100

    Render test

--- src/main/java/wm/gurpswm/Firebase/FirebaseConfig.java
+++ src/main/java/wm/gurpswm/Firebase/FirebaseConfig.java
@@ -1,5 +1,6 @@
 package wm.gurpswm.Firebase;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -16,7 +17,6 @@ public class FirebaseConfig {
     private static final String FB_KEY = "AIzaSyBPymmztNuVSLdjXo1BWyr7dDleE7X9qE4";
     private static final String IDTOOLKIT_URL = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=";
 
-    @SuppressWarnings("unused")
     @Bean
     public FirebaseApp fbApp() throws IOException {
         FirebaseApp app = null;
@@ -24,11 +24,20 @@ public class FirebaseConfig {
             app = FirebaseApp.getInstance("DEFAULT");
         }
         catch (IllegalStateException e) {
-            InputStream serviceAccount = getClass().getClassLoader().getResourceAsStream("firebase-config.json");
-            
-            FirebaseOptions options = FirebaseOptions.builder()
-            .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-            .build();
+            FirebaseOptions options = null;
+            if (new File("./src/main/resources/firebase-config.json").exists()) {
+                InputStream serviceAccount = getClass().getClassLoader().getResourceAsStream("firebase-config.json");
+                
+                options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+            } else {
+                options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.getApplicationDefault())
+                .build();
+            }
+
+            app = FirebaseApp.initializeApp(options);
         }
 
         return app;
