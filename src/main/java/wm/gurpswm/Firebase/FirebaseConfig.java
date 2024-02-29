b5355e7fe7696a55de5801d17859e6c690df3afe
Fersanseg <fer_ssa94@hotmail.com>
2024-02-29 19:40:14 +0100

    fb

--- src/main/java/wm/gurpswm/Firebase/FirebaseConfig.java
+++ src/main/java/wm/gurpswm/Firebase/FirebaseConfig.java
@@ -4,12 +4,13 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+
+import jakarta.annotation.PostConstruct;
 
 
 @Configuration
@@ -17,30 +18,22 @@ public class FirebaseConfig {
     private static final String FB_KEY = "AIzaSyBPymmztNuVSLdjXo1BWyr7dDleE7X9qE4";
     private static final String IDTOOLKIT_URL = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=";
 
-    @Bean
-    public FirebaseApp fbApp() throws IOException {
-        FirebaseApp app = null;
-        try {
-            app = FirebaseApp.getInstance("DEFAULT");
-        }
-        catch (IllegalStateException e) {
-            FirebaseOptions options = null;
-            if (new File("./src/main/resources/firebase-config.json").exists()) {
-                InputStream serviceAccount = getClass().getClassLoader().getResourceAsStream("firebase-config.json");
-                
-                options = FirebaseOptions.builder()
+    @PostConstruct
+    public void fbInit() throws IOException {
+        FirebaseOptions options = null;
+        if (new File("./src/main/resources/firebase-config.json").exists()) {
+            InputStream serviceAccount = getClass().getClassLoader().getResourceAsStream("firebase-config.json");
+            
+            options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                 .build();
-            } else {
-                options = FirebaseOptions.builder()
+        } else {
+            options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.getApplicationDefault())
                 .build();
-            }
-
-            app = FirebaseApp.initializeApp(options);
         }
 
-        return app;
+        FirebaseApp.initializeApp(options);
     }
 
     public static String getTokenUrl() {
